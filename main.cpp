01047c4b1fbbd7bc8fd230ec4567b6228b99fb5b
lwllza <241279897+lwllza@users.noreply.github.com>
2026-02-25 09:02:09 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+
+int main() {
+    std::cout << "Программа запущена" << std::endl;
+    return 0;
+}
+
+// Новая функция для математических операций
+int add(int a, int b) {
+    return a + b;
+}
